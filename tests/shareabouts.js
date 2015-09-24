745d742d2d7be2379101fdc0b100f617ffdf6f31
Luke Swart <lukeswart@gmail.com>
2015-09-24 03:02:39 -0700

    Add basic places tests

--- tests/shareabouts.js
+++ tests/shareabouts.js
@@ -2,6 +2,7 @@ var test = require('tape')
 var config = require('../config')
 var auth = require('../lib/shareabouts/auth')(config)
 var datasets = require('../lib/shareabouts/datasets')(config)
+var places = require('../lib/shareabouts/places')(config)
 
 test('log in using username & password', function (t) {
   auth.logIn({ username: 'admin', password: 'sodasoda' }, function (err, res) {
@@ -27,6 +28,8 @@ test('try to get current user', function (t) {
 test('get all datasets', function (t) {
   datasets.list({ username: 'admin', password: 'sodasoda' }, function (err, res) {
     console.log(err, res)
+    t.notOk(err)
+    t.ok(res)
     t.end()
   })
 })
@@ -34,6 +37,27 @@ test('get all datasets', function (t) {
 test('get one dataset', function (t) {
   datasets.get({ slug: 'example', username: 'admin', password: 'sodasoda' }, function (err, res) {
     console.log(err, res)
+    t.notOk(err)
+    t.ok(res)
+    t.end()
+  })
+})
+
+test('get places from example dataset', function (t) {
+  places.list({ username: 'admin', slug: 'example' }, function (err, res) {
+    // console.log(err, res)
+    t.notOk(err)
+    t.ok(res)
+    t.end()
+  })
+})
+
+// TODO: POST a new place, and use that placeId for this test:
+test('get a place from the example dataset', function (t) {
+  places.get({ username: 'admin', slug: 'example', placeId: 3263 }, function (err, res) {
+    console.log(err, res)
+    t.notOk(err)
+    t.ok(res)
     t.end()
   })
 })
