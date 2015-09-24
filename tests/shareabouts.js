2f8746ef59bd61ef3334f769361b7caef8683796
Luke Swart <lukeswart@gmail.com>
2015-09-24 15:06:48 -0700

    Add new place POST with test

--- tests/shareabouts.js
+++ tests/shareabouts.js
@@ -52,7 +52,36 @@ test('get places from example dataset', function (t) {
   })
 })
 
-// TODO: POST a new place, and use that placeId for this test:
+// TODO: create a place that specifies an ID (requires special privileges on the dataset)
+test('post a place to the example dataset', function (t) {
+  places.post({ username: 'admin', password: 'sodasoda', slug: 'example', placeId: 3263,
+                body:
+      {
+        "properties": {
+          "description": "asdf2",
+          "location_type": "observation",
+          "private-submitter_email": "asd2",
+          "name": "asdf2",
+          "user_token": "user:7",
+          "visible": "on"
+        },
+        "geometry": {
+          "coordinates": [
+              -122.5,
+            47.51951934150781
+          ],
+          "type": "Point"
+        },
+        "type": "Feature"
+      }
+              }, function (err, res) {
+    console.log(err, res)
+    t.notOk(err)
+    t.ok(res)
+    t.end()
+  })
+})
+
 test('get a place from the example dataset', function (t) {
   places.get({ username: 'admin', slug: 'example', placeId: 3263 }, function (err, res) {
     console.log(err, res)
