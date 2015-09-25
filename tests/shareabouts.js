496313e84762871257f91d3ec6ce9ed125d05e80
Luke Swart <lukeswart@gmail.com>
2015-09-25 15:33:00 -0700

    Places and dataset tests for api-client
     - Add TEST_DATASET and TEST_HOST constants
     - Implement create, update, delete for datasets and places

--- tests/shareabouts.js
+++ tests/shareabouts.js
@@ -4,6 +4,11 @@ var auth = require('../lib/shareabouts/auth')(config)
 var datasets = require('../lib/shareabouts/datasets')(config)
 var places = require('../lib/shareabouts/places')(config)
 
+// Our dataset for testing, which should not already exist
+var TEST_DATASET = 'blahBlahExampleDataset'
+var TEST_HOST = config.host || 'http://127.0.0.1:8001/api/v2/'
+
+// Assume that we already have a superuser names 'admin' with password 'sodasoda'
 test('log in using username & password', function (t) {
   auth.logIn({ username: 'admin', password: 'sodasoda' }, function (err, res) {
     t.ok(res)
@@ -26,6 +31,23 @@ test('try to get current user', function (t) {
   })
 })
 
+// TODO: Only create the new test dataset if it doesn't already exist
+// (helpful if the test fails after we create the dataset)
+test('create a new test dataset', function (t) {
+  datasets.post({ username: 'admin', password: 'sodasoda',
+                  body: {
+                    "display_name": TEST_DATASET,
+                    "slug": TEST_DATASET,
+                    "owner": TEST_HOST + 'admin'
+                  }
+                  }, function (err, res) {
+    console.log(err, res)
+    t.notOk(err)
+    t.ok(res)
+    t.end()
+  })
+})
+
 test('get all datasets', function (t) {
   datasets.list({ username: 'admin', password: 'sodasoda' }, function (err, res) {
     console.log(err, res)
@@ -36,57 +58,109 @@ test('get all datasets', function (t) {
 })
 
 test('get one dataset', function (t) {
-  datasets.get({ slug: 'example', username: 'admin', password: 'sodasoda' }, function (err, res) {
+  datasets.get({ slug: TEST_DATASET, username: 'admin', password: 'sodasoda' }, function (err, res) {
     console.log(err, res)
     t.notOk(err)
     t.ok(res)
     t.end()
   })
+})
+
+test('Update our dataset to contain special permissions', function (t) {
+  datasets.put({ username: 'admin', password: 'sodasoda', slug: TEST_DATASET,
+                 body: {
+                   "display_name": "new display name",
+                   "slug": TEST_DATASET,
+                   "owner": TEST_HOST + 'admin'
+                 }
+               }, function (err, res) {
+                 t.notOk(err)
+                 t.ok(res)
+                 t.end()
+               })
 })
 
 test('get places from example dataset', function (t) {
-  places.list({ username: 'admin', slug: 'example' }, function (err, res) {
+  places.list({ username: 'admin', slug: TEST_DATASET}, function (err, res) {
     t.notOk(err)
     t.ok(res)
     t.end()
   })
 })
 
-// TODO: create a place that specifies an ID (requires special privileges on the dataset)
+var examplePlaceId;
 test('post a place to the example dataset', function (t) {
-  places.post({ username: 'admin', password: 'sodasoda', slug: 'example', placeId: 3263,
-                body:
-      {
-        "properties": {
-          "description": "asdf2",
-          "location_type": "observation",
-          "private-submitter_email": "asd2",
-          "name": "asdf2",
-          "user_token": "user:7",
-          "visible": "on"
-        },
-        "geometry": {
-          "coordinates": [
-              -122.5,
-            47.51951934150781
-          ],
-          "type": "Point"
-        },
-        "type": "Feature"
-      }
+  places.post({ username: 'admin', password: 'sodasoda', slug: TEST_DATASET,
+                body: {
+                  "properties": {
+                    "description": "asdf2",
+                    "location_type": "observation",
+                    "private-submitter_email": "asd2",
+                    "name": "asdf2",
+                    "user_token": "user:7",
+                    "visible": "on"
+                  },
+                  "geometry": {
+                    "coordinates": [
+                        -122.5,
+                      47.51951934150781
+                    ],
+                    "type": "Point"
+                  },
+                  "type": "Feature"
+                }
               }, function (err, res) {
+                console.log(err, res)
+                t.notOk(err)
+                t.ok(res)
+                examplePlaceId = res.id
+                console.log("examplePlaceId:", examplePlaceId)
+                t.end()
+              })
+})
+
+test('update our previously added place to the example dataset', function (t) {
+  places.put({ username: 'admin', password: 'sodasoda', slug: TEST_DATASET, placeId: examplePlaceId,
+               body: {
+                 "properties": {
+                   "description": "asdf4",
+                   "location_type": "observation",
+                   "private-submitter_email": "asd4",
+                   "name": "asdf4",
+                   "user_token": "user:7",
+                   "visible": "on"
+                 },
+                 "geometry": {
+                   "coordinates": [
+                       -122.5,
+                     47.51951934150781
+                   ],
+                   "type": "Point"
+                 },
+                 "type": "Feature"
+               }
+             }, function (err, res) {
+               console.log(err, res)
+               t.notOk(err)
+               t.ok(res)
+               t.end()
+             })
+})
+
+test('delete an existing place to the example dataset', function (t) {
+  places.delete({ username: 'admin', password: 'sodasoda', slug: TEST_DATASET, placeId: examplePlaceId }, function (err, res) {
     console.log(err, res)
     t.notOk(err)
-    t.ok(res)
+    t.ok(res === '')
     t.end()
   })
 })
 
-test('get a place from the example dataset', function (t) {
-  places.get({ username: 'admin', slug: 'example', placeId: 3263 }, function (err, res) {
+test('delete our test dataset', function (t) {
+  datasets.delete({ username: 'admin', password: 'sodasoda', slug: TEST_DATASET }, function (err, res) {
     console.log(err, res)
     t.notOk(err)
-    t.ok(res)
+    t.ok(res === '')
     t.end()
   })
 })
