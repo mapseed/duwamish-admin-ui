73e2899caf6377abe047395aa0f559fd88011de7
sethvincent <sethvincent@gmail.com>
2015-09-25 17:53:40 -0700

    add test-browser script

--- config.js
+++ config.js
@@ -2,6 +2,9 @@ var config = {
   development: {
     host: 'http://127.0.0.1:8001/api/v2/'
   },
+  staging: {
+    host: 'http://dev-api.heyduwamish.org:8010/api/v2/'
+  },
   production: {
     host: null
   }
