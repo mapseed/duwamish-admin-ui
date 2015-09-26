ce638eb8d86222630ccc7aa4f2c8866b25c7278e
sethvincent <sethvincent@gmail.com>
2015-09-25 20:27:40 -0700

    troubleshooting cors issues

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config = {
     host: 'http://127.0.0.1:8001/api/v2/'
   },
   staging: {
-    host: 'http://dev-api.heyduwamish.org:8010/api/v2/'
+    host: 'http://dev-api.heyduwamish.org:8009/api/v2/',
   },
   production: {
     host: null
--- tests/shareabouts.js
+++ tests/shareabouts.js
@@ -5,9 +5,8 @@ var datasets = require('../lib/shareabouts/datasets')(config)
 var places = require('../lib/shareabouts/places')(config)
 
 // Our dataset for testing, which should not already exist
-var TEST_DATASET = config.dataset || 'blahBlahExampleDataset'
+var TEST_DATASET = config.dataset || 'example'
 var TEST_HOST = config.host || 'http://127.0.0.1:8001/api/v2/'
-console.log("config.host:", config.host)
 var USERNAME = config.username || 'admin'
 var PASSWORD = config.password || 'sodasoda'
 
@@ -34,22 +33,24 @@ test('try to get current user', function (t) {
   })
 })
 
-// // TODO: Only create the new test dataset if it doesn't already exist
-// // (helpful if the test fails after we create the dataset)
-// test('create a new test dataset', function (t) {
-//   datasets.post({ username: USERNAME, password: PASSWORD,
-//                   body: {
-//                     "display_name": TEST_DATASET,
-//                     "slug": TEST_DATASET,
-//                     "owner": TEST_HOST + USERNAME
-//                   }
-//                   }, function (err, res) {
-//     console.log(err, res)
-//     t.notOk(err)
-//     t.ok(res)
-//     t.end()
-//   })
-// })
+// TODO: Only create the new test dataset if it doesn't already exist
+// (helpful if the test fails after we create the dataset)
+test('create a new test dataset', function (t) {
+  datasets.post({
+    username: USERNAME,
+    password: PASSWORD,
+    body: {
+      "display_name": TEST_DATASET,
+      "slug": TEST_DATASET,
+      "owner": TEST_HOST + USERNAME
+    }
+  }, function (err, res) {
+    console.log(err, res)
+    t.notOk(err)
+    t.ok(res)
+    t.end()
+  })
+})
 
 test('get all datasets', function (t) {
   datasets.list({ username: USERNAME, password: PASSWORD }, function (err, res) {
@@ -69,22 +70,22 @@ test('get one dataset', function (t) {
   })
 })
 
-// test('Update our dataset to contain special permissions', function (t) {
-//   datasets.put({ username: USERNAME, password: PASSWORD, slug: TEST_DATASET,
-//                  body: {
-//                    "display_name": "new display name",
-//                    "slug": TEST_DATASET,
-//                    "owner": TEST_HOST + USERNAME
-//                  }
-//                }, function (err, res) {
-//                  t.notOk(err)
-//                  t.ok(res)
-//                  t.end()
-//                })
-// })
+test('Update our dataset to contain special permissions', function (t) {
+  datasets.put({ username: USERNAME, password: PASSWORD, slug: TEST_DATASET,
+                 body: {
+                   "display_name": "new display name",
+                   "slug": TEST_DATASET,
+                   "owner": TEST_HOST + USERNAME
+                 }
+               }, function (err, res) {
+                 t.notOk(err)
+                 t.ok(res)
+                 t.end()
+               })
+})
 
 test('get places from test dataset', function (t) {
-  places.list({ username: USERNAME, slug: TEST_DATASET}, function (err, res) {
+  places.list({ username: USERNAME, slug: TEST_DATASET }, function (err, res) {
     console.log("res:", res)
     t.notOk(err)
     t.ok(res)
@@ -92,79 +93,88 @@ test('get places from test dataset', function (t) {
   })
 })
 
-// var examplePlaceId;
-// test('post a place to the example dataset', function (t) {
-//   places.post({ username: USERNAME, password: PASSWORD, slug: TEST_DATASET,
-//                 body: {
-//                   "properties": {
-//                     "description": "asdf2",
-//                     "location_type": "observation",
-//                     "private-submitter_email": "asd2",
-//                     "name": "asdf2",
-//                     "user_token": "user:7",
-//                     "visible": "on"
-//                   },
-//                   "geometry": {
-//                     "coordinates": [
-//                         -122.5,
-//                       47.51951934150781
-//                     ],
-//                     "type": "Point"
-//                   },
-//                   "type": "Feature"
-//                 }
-//               }, function (err, res) {
-//                 console.log(err, res)
-//                 t.notOk(err)
-//                 t.ok(res)
-//                 examplePlaceId = res.id
-//                 console.log("examplePlaceId:", examplePlaceId)
-//                 t.end()
-//               })
-// })
+test('get a place', function (t) {
+  places.get({ username: USERNAME, slug: TEST_DATASET, placeId: '3269' }, function (err, res) {
+    console.log(err, res)
+    t.notOk(err)
+    t.ok(res)
+    t.end()
+  })
+})
 
-// test('update our previously added place to the example dataset', function (t) {
-//   places.put({ username: USERNAME, password: PASSWORD, slug: TEST_DATASET, placeId: examplePlaceId,
-//                body: {
-//                  "properties": {
-//                    "description": "asdf4",
-//                    "location_type": "observation",
-//                    "private-submitter_email": "asd4",
-//                    "name": "asdf4",
-//                    "user_token": "user:7",
-//                    "visible": "on"
-//                  },
-//                  "geometry": {
-//                    "coordinates": [
-//                        -122.5,
-//                      47.51951934150781
-//                    ],
-//                    "type": "Point"
-//                  },
-//                  "type": "Feature"
-//                }
-//              }, function (err, res) {
-//                console.log(err, res)
-//                t.notOk(err)
-//                t.ok(res)
-//                t.end()
-//              })
-// })
+var examplePlaceId;
+test('post a place to the example dataset', function (t) {
+  places.post({ username: USERNAME, password: PASSWORD, slug: TEST_DATASET,
+                body: {
+                  "properties": {
+                    "description": "asdf2",
+                    "location_type": "observation",
+                    "private-submitter_email": "asd2",
+                    "name": "asdf2",
+                    "user_token": "user:7",
+                    "visible": "on"
+                  },
+                  "geometry": {
+                    "coordinates": [
+                        -122.5,
+                      47.51951934150781
+                    ],
+                    "type": "Point"
+                  },
+                  "type": "Feature"
+                }
+              }, function (err, res) {
+                console.log(err, res)
+                t.notOk(err)
+                t.ok(res)
+                examplePlaceId = res.id
+                console.log("examplePlaceId:", examplePlaceId)
+                t.end()
+              })
+})
 
-// test('delete an existing place to the example dataset', function (t) {
-//   places.delete({ username: USERNAME, password: PASSWORD, slug: TEST_DATASET, placeId: examplePlaceId }, function (err, res) {
-//     console.log(err, res)
-//     t.notOk(err)
-//     t.ok(res === '')
-//     t.end()
-//   })
-// })
+test('update our previously added place to the example dataset', function (t) {
+  places.put({ username: USERNAME, password: PASSWORD, slug: TEST_DATASET, placeId: examplePlaceId,
+               body: {
+                 "properties": {
+                   "description": "asdf4",
+                   "location_type": "observation",
+                   "private-submitter_email": "asd4",
+                   "name": "asdf4",
+                   "user_token": "user:7",
+                   "visible": "on"
+                 },
+                 "geometry": {
+                   "coordinates": [
+                       -122.5,
+                     47.51951934150781
+                   ],
+                   "type": "Point"
+                 },
+                 "type": "Feature"
+               }
+             }, function (err, res) {
+               console.log(err, res)
+               t.notOk(err)
+               t.ok(res)
+               t.end()
+             })
+})
 
-// test('delete our test dataset', function (t) {
-//   datasets.delete({ username: USERNAME, password: PASSWORD, slug: TEST_DATASET }, function (err, res) {
-//     console.log(err, res)
-//     t.notOk(err)
-//     t.ok(res === '')
-//     t.end()
-//   })
-// })
+test('delete an existing place to the example dataset', function (t) {
+  places.delete({ username: USERNAME, password: PASSWORD, slug: TEST_DATASET, placeId: examplePlaceId }, function (err, res) {
+    console.log(err, res)
+    t.notOk(err)
+    t.ok(res === '')
+    t.end()
+  })
+})
+
+test('delete our test dataset', function (t) {
+  datasets.delete({ username: USERNAME, password: PASSWORD, slug: TEST_DATASET }, function (err, res) {
+    console.log(err, res)
+    t.notOk(err)
+    t.ok(res === '')
+    t.end()
+  })
+})
