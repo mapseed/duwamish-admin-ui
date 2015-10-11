b3d46d59e0b19cdcc4fd174ed1a47d30a0b4e03c
Luke Swart <lukeswart@gmail.com>
2015-10-11 04:19:13 -0700

    Add check to ensure our place is equal
     - Add subset module to check that origin json place is a subset of what was returned by API
     - Enable use of GET method for private places
     - Move our place body into a variable (perhaps load it from a config later?)

--- tests/shareabouts.js
+++ tests/shareabouts.js
@@ -1,5 +1,6 @@
 var test = require('tape')
 var config = require('../config')
+var subset = require('../lib/json-subset')
 var auth = require('../lib/shareabouts/auth')(config)
 var datasets = require('../lib/shareabouts/datasets')(config)
 var places = require('../lib/shareabouts/places')(config)
@@ -34,6 +35,7 @@ test('try to get current user', function (t) {
 })
 
 // TODO: Only create the new test dataset if it doesn't already exist
+// and/or delete the test dataset if the test fails
 // (helpful if the test fails after we create the dataset)
 test('create a new test dataset', function (t) {
   datasets.post({
@@ -93,27 +95,28 @@ test('get places from test dataset', function (t) {
   })
 })
 
-var examplePlaceId;
+var placeBody = {
+  "properties": {
+    "description": "my description is here",
+    "location_type": "observation",
+    "private-submitter_email": "email@email.com",
+    "name": "hey Joe",
+    "user_token": "user:7",
+    "visible": true
+  },
+  "geometry": {
+    "coordinates": [
+        -122.5,
+      47.51951934150781
+    ],
+    "type": "Point"
+  },
+  "type": "Feature"
+}
+var examplePlaceId
 test('post a place to the example dataset', function (t) {
   places.post({ username: USERNAME, password: PASSWORD, slug: TEST_DATASET,
-                body: {
-                  "properties": {
-                    "description": "asdf2",
-                    "location_type": "observation",
-                    "private-submitter_email": "asd2",
-                    "name": "asdf2",
-                    "user_token": "user:7",
-                    "visible": "on"
-                  },
-                  "geometry": {
-                    "coordinates": [
-                        -122.5,
-                      47.51951934150781
-                    ],
-                    "type": "Point"
-                  },
-                  "type": "Feature"
-                }
+                body: placeBody
               }, function (err, res) {
                 console.log(err, res)
                 t.notOk(err)
@@ -124,41 +127,41 @@ test('post a place to the example dataset', function (t) {
               })
 })
 
+// Checking to ensure that our place is the same
+// NOTE: We are using JSON.parse because Shareabouts API (or our request module) is not returning valid JSON
 test('get a place', function (t) {
-  places.get({ username: USERNAME, slug: TEST_DATASET, placeId: examplePlaceId }, function (err, res) {
+  places.getPrivate({ username: USERNAME, password: PASSWORD, slug: TEST_DATASET, placeId: examplePlaceId }, function (err, res) {
     console.log(err, res)
     t.notOk(err)
     t.ok(res)
+    t.ok(subset(placeBody, JSON.parse(res)))
     t.end()
   })
 })
 
+placeBody['properties'].description = "I am changing my description"
 test('update our previously added place to the example dataset', function (t) {
   places.put({ username: USERNAME, password: PASSWORD, slug: TEST_DATASET, placeId: examplePlaceId,
-               body: {
-                 "properties": {
-                   "description": "asdf4",
-                   "location_type": "observation",
-                   "private-submitter_email": "asd4",
-                   "name": "asdf4",
-                   "user_token": "user:7",
-                   "visible": "on"
-                 },
-                 "geometry": {
-                   "coordinates": [
-                       -122.5,
-                     47.51951934150781
-                   ],
-                   "type": "Point"
-                 },
-                 "type": "Feature"
-               }
+               body: placeBody
              }, function (err, res) {
                console.log(err, res)
                t.notOk(err)
                t.ok(res)
                t.end()
              })
+})
+
+/*
+ * Checking whether our update was successful
+ */
+test('get a place', function (t) {
+  places.getPrivate({ username: USERNAME, password: PASSWORD, slug: TEST_DATASET, placeId: examplePlaceId }, function (err, res) {
+    console.log(err, res)
+    t.notOk(err)
+    t.ok(res)
+    t.ok(subset(placeBody, JSON.parse(res)))
+    t.end()
+  })
 })
 
 test('delete an existing place to the example dataset', function (t) {
